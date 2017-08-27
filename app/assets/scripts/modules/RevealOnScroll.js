652898f833f094bec77bfebbd803ca027bdf153b
kapten1428 <kapten1428@gmail.com>
2017-08-27 09:30:11 +0700

    Reveal item for features implemented

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,5 +1,31 @@
-class RevealOnScroll{
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
+
+class RevealOnScroll{
+	constructor(){
+		this.itemsToReveal = $(".feature-item");
+		this.hideInitially();
+		this.createWaypoints();
+	}
+
+	hideInitially(){
+		this.itemsToReveal.addClass("reveal-item");
+	}
+
+	createWaypoints(){
+		this.itemsToReveal.each(function(){
+			var currentItem = this;
+			new Waypoint({
+				element :  currentItem,
+				handler : function(){
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset : "60%"
+
+			})
+		});
+	}
 }
 
 export default RevealOnScroll;
